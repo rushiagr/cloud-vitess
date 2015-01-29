2959e0a2cb15804bfab84ababaf49497fbc404dd
Rushi Agrawal <rushi.agr@gmail.com>
2015-01-29 10:29:48 +0000

    Add setup-vt.sh

---
+++ setup-vt.sh
@@ -0,0 +1,38 @@
+#! /bin/bash -x
+
+cd ~/src/github.com/youtube/vitess/
+
+. ./dev.env
+
+cd ~/src/github.com/youtube/vitess/examples/local
+
+#./zk-up.sh
+
+sleep 5
+
+#./vtctld-up.sh
+
+sleep 5
+
+$VTROOT/bin/vtctlclient -server localhost:15000
+
+#./vttablet-up.sh
+
+sleep 5
+
+$VTROOT/bin/vtctlclient -server localhost:15000 RebuildKeyspaceGraph test_keyspace
+
+sleep 5
+
+
+$VTROOT/bin/vtctlclient -server localhost:15000 ReparentShard -force test_keyspace/0 test-0000000100
+
+sleep 5
+
+$VTROOT/bin/vtctlclient -server localhost:15000 ListAllTablets test
+
+sleep 5
+
+$VTROOT/bin/vtctlclient -server localhost:15000 ApplySchemaKeyspace -simple -sql "$(cat create_test_table.sql)" test_keyspace
+
+
