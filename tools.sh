63971b5a6a449ac2436353f98bc8d6c983b0b977
Rushi Agrawal <rushi.agr@gmail.com>
2015-01-30 06:10:27 +0000

    Add 'vttablet' function to tools.sh
    note that the function doesn't currently work

---
+++ tools.sh
@@ -0,0 +1,34 @@
+# Create a vttablet by passing a number between 100 and 999
+# NOTE: doesnt work now.
+
+vttablet() {
+    if [ -z $2 ]; then
+        echo "Usage: vttablet master 100"
+        exit 1
+    fi
+
+    /home/vagrant/bin/vttablet \
+        -log_dir /home/vagrant/vtdataroot/tmp \
+        -port 15$2 \
+        -db-config-app-uname vt_app \
+        -db-config-app-dbname vt_test_keyspace \
+        -db-config-app-charset utf8 \
+        -db-config-dba-uname vt_dba \
+        -db-config-dba-charset utf8 \
+        -db-config-repl-uname vt_repl \
+        -db-config-repl-dbname vt_test_keyspace \
+        -db-config-repl-charset utf8 \
+        -db-config-filtered-uname vt_filtered \
+        -db-config-filtered-dbname vt_test_keyspace \
+        -db-config-filtered-charset utf8 \
+        -tablet-path test-0000000$2 \
+        -init_keyspace test_keyspace \
+        -init_shard 0
+        -target_tablet_type $1 \
+        -enable-rowcache \
+        -rowcache-bin /usr/bin/memcached \
+        -rowcache-socket /home/vagrant/vtdataroot/vt_0000000$2/memcache.sock \
+        > /home/vagrant/vtdataroot/vt_0000000$2/vttablet.out 2>&1 &
+
+    disown -a
+}
